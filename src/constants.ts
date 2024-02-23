7c4759db80d59eff14339d9735916020076e6bca
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2024-02-23 13:28:55 +1100

    increase particle size

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const SCREEN_COLS = 50;
 export const SCREEN_ROWS = 50;
 
 // width and height of each grid location (table cell) in pixels
-export const PARTICLE_SIZE = 7;
+export const PARTICLE_SIZE = 8;
 
 export const OUT_OF_BOUNDS = -1;
 
